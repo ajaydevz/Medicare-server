665f95d4bbce836e461c52a9492447a0c535ce6c
ajaydev <ajaydevzzz30@gmail.com>
2024-07-21 19:46:34 +0530

    fetch doctors

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("register", RegisterView.as_view(), name="register"),
@@ -12,3 +13,6 @@ urlpatterns = [
 
    
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
